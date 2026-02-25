a76bc2bb9efd7a076082df5d28d399220dc08002
Nacho Morilla Vargas <nachomorillavargas@MacBook-Pro-de-Nacho.local>
2026-02-26 00:46:15 +0100

    Connect to PostgreSQL

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
-const app = express();
+const { Pool } = require("pg");
 
-app.get("/", (req, res) => {
-  res.send("MiTV Pro backend funcionando 🚀");
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      message: "MiTV Pro backend conectado a PostgreSQL 🚀",
+      time: result.rows[0].now,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error conectando a la base de datos");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
